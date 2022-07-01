da29efbdce183bb54d300e0beb881affd2da09ac
Michelreisdev <michelreisdev@gmail.com>
2022-07-01 09:56:57 -0300

    Ajustes de codificação utf8

--- lib/modal/class.politico.dart
+++ lib/modal/class.politico.dart
@@ -1,19 +1,17 @@
-import 'dart:convert';
+import 'dart:convert' show jsonDecode, utf8;
 import 'dart:developer';
 import 'package:flutter/cupertino.dart';
 import 'class.api_parlamentar.dart';
 import 'package:http/http.dart' as http;
+
 
 class Politico extends ChangeNotifier {
 
   List<dynamic> parlamentars = [];
 
   Politico(){
-    this.getParlamento();
-   
+    getParlamento();
   }
-
-  
 
   Future<dynamic> getParlamento() async {
     var headers = {'Accept': 'application/json'};
@@ -28,7 +26,7 @@ class Politico extends ChangeNotifier {
     var response = await http.Response.fromStream(streamedResponse);
 
     if (response.statusCode == 200) {
-      var jsons =   jsonDecode( response.body.toString() );
+      var jsons = jsonDecode(utf8.decode(response.bodyBytes));
       try{
         parlamentars = jsons['ListaParlamentarEmExercicio']['Parlamentares']['Parlamentar'].map((job) => Parlamentar.fromJson(job)).toList();
       }catch(e){

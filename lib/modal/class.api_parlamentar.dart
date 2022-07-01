54edc032ac55d94153d5ebdfa262f90c5b71ae12
michelreisdev <michelreisdev@gmail.com>
2022-07-01 02:23:41 -0300

    init

--- lib/modal/class.api_parlamentar.dart
+++ lib/modal/class.api_parlamentar.dart
@@ -1,14 +1,20 @@
 class Parlamentar {
   final String NomeCompletoParlamentar;
+  final String UrlFotoParlamentar;
+  final String SiglaPartidoParlamentar;
 
 
   const Parlamentar({
     required this.NomeCompletoParlamentar,
+    required this.UrlFotoParlamentar,
+    required this.SiglaPartidoParlamentar,
   });
 
   factory Parlamentar.fromJson(Map json) {
     return Parlamentar(
       NomeCompletoParlamentar: json['IdentificacaoParlamentar']['NomeCompletoParlamentar'] as String,
+      UrlFotoParlamentar: json['IdentificacaoParlamentar']['UrlFotoParlamentar'] as String,
+      SiglaPartidoParlamentar: json['IdentificacaoParlamentar']['SiglaPartidoParlamentar'] as String,
     );
   }
 }
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -22,28 +22,71 @@ class Home extends StatelessWidget {
       appBar: AppBar(),
       body: Consumer<Politico>(builder: (_,politico,___){
         inspect(politico.parlamentars);
-        return Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text("ok"),
-          ),
-          Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextButton(
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
-                ),
-                onPressed: () async {
-  
-                  
-                },
-                child: const Text('Buscar'),
-              ))
-        ],
-      );
+        return ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: politico.parlamentars.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                child: Container(
+                  height: 100.0,
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 100.0,
+                        width: 70.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(5),
+                            topLeft: Radius.circular(5)
+                          ),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(politico.parlamentars[index].UrlFotoParlamentar)
+                          )
+                        ),
+                      ),
+                      Container(
+                          height: 100,
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(10, 2, 0, 0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text('${politico.parlamentars[index].NomeCompletoParlamentar}'),
+                                Padding(
+                                    padding: EdgeInsets.fromLTRB(0, 3, 0, 3),
+                                    child: Container(
+                                    width: 92,
+                                    decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.teal),
+                                      borderRadius: BorderRadius.all(Radius.circular(10))
+                                    ),
+                                    child: Text('${politico.parlamentars[index].SiglaPartidoParlamentar}',textAlign: TextAlign.center,),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 5, 0, 2),
+                                    child: Container(
+                                    width: 260,
+                                    child: Text("__",style: TextStyle(
+                                      fontSize: 15,
+                                      color: Color.fromARGB(255, 48, 48, 54)
+                                    ),),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                    ],
+                  ),
+                )
+              );
+            }
+          );
       },),
     );
   }
 }
+
+/* Center(child: Text('${politico.parlamentars[index].NomeCompletoParlamentar}')) */
